f97566f0daf48cc6262b50dd5cd2d2aefbb9e78c
VishnuVaradhanVS <115619164+VishnuVaradhanVS@users.noreply.github.com>
2025-08-30 14:50:12 +0530

    Update gittest.java

--- gittest.java
+++ gittest.java
@@ -2,5 +2,7 @@ public class gittest{
     public static void main(String[] args) {
         System.err.println("Hi");
         System.System.out.println("HIIII");
+
+        Sysetm.out.println('Eifslidjf");
     }
 }
